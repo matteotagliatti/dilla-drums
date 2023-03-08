0a124d7fe35d63e8522a18677f8fff712ce22fab
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-03-08 14:16:27 +0100

    update style

--- src/components/Device.jsx
+++ src/components/Device.jsx
@@ -2,8 +2,8 @@ import Sticker from "./Sticker";
 
 export default function Device() {
   return (
-    <div className="w-96 flex flex-col border-4 border-slate-300 rounded-lg bg-gradient-to-r from-slate-200 via-slate-100 to-slate-200 shadow-lg">
-      <div className="w-full pt-5 p-3 bg-slate-100 rounded-md border-2 border-slate-200 z-10">
+    <div className="w-96 flex flex-col border-4 border-neutral-300 rounded-lg bg-gradient-to-r from-neutral-200 via-neutral-100 to-neutral-200 shadow-lg">
+      <div className="w-full pt-5 p-3 bg-neutral-100 border-2 border-neutral-200 rounded-md z-10">
         <div className="grid grid-cols-4 w-full gap-2">
           <div className="w-full py-2 col-span-4 flex justify-center">
             <Sticker />
