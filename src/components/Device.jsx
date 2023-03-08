679684b409b548f10cead0e2fc382012de9aecd9
Matteo Tagliatti <matteotagliatti@gmail.com>
2023-03-08 14:53:53 +0100

    add switch

--- src/components/Device.jsx
+++ src/components/Device.jsx
@@ -1,12 +1,44 @@
+import { useState } from "react";
+import { Howl } from "howler";
 import Sticker from "./Sticker";
+import Switch from "./Switch";
+import { HipHop, CasioPT30, RealDrums, TrapDrums } from "./Sounds";
 
 export default function Device() {
+  const [activeSounds, setActiveSounds] = useState(HipHop);
+
+  const click = (active) => {
+    const sound = new Howl({
+      src: [`/audio/click.mp3`],
+    });
+    sound.play();
+    setActiveSounds(active);
+  };
+
+  const sounds = [
+    { name: "Beat", sounds: HipHop, keypress: "1" },
+    { name: "Casio", sounds: CasioPT30, keypress: "2" },
+    { name: "Real", sounds: RealDrums, keypress: "3" },
+    { name: "Trap", sounds: TrapDrums, keypress: "4" },
+  ];
+
   return (
-    <div className="w-96 flex flex-col border-4 border-neutral-300 rounded-lg bg-gradient-to-r from-neutral-200 via-neutral-100 to-neutral-200 shadow-lg">
-      <div className="w-full pt-5 p-3 bg-neutral-100 border-2 border-neutral-200 rounded-md z-10">
+    <div className="w-96 flex flex-col border-4 border-slate-300 rounded-lg bg-gradient-to-r from-slate-200 via-slate-100 to-slate-200 shadow-lg">
+      <div className="w-full pt-5 p-3 bg-slate-100 border-2 border-slate-200 rounded-md z-10">
         <div className="grid grid-cols-4 w-full gap-2">
           <div className="w-full py-2 col-span-4 flex justify-center">
             <Sticker />
+          </div>
+          <div className="col-span-4 px-2 pt-2 pb-0 bg-slate-600 rounded-md grid grid-cols-4 gap-2">
+            {sounds.map((item, i) => (
+              <Switch
+                item={item}
+                key={i}
+                keyPress={item.keypress}
+                play={() => click(item.sounds)}
+                active={activeSounds == item.sounds}
+              />
+            ))}
           </div>
         </div>
       </div>
---
+++ src/components/Switch.jsx
@@ -0,0 +1,35 @@
+import useKeypress from "react-use-keypress";
+
+export default function Switch({ item, active, play, keyPress }) {
+  const handleClick = () => {
+    play();
+  };
+
+  useKeypress(keyPress, () => {
+    play();
+  });
+
+  return (
+    <div className={`relative flex flex-col`}>
+      <button
+        className={`flex items-center justify-between relative z-10 transition w-full px-3 py-2 rounded-md font-semibold text-xs uppercase tracking-widest text-slate-600 ${
+          active
+            ? "top-0 shadow-inner bg-slate-300 ring-4 ring-black ring-opacity-10"
+            : "-top-1 bg-slate-200 shadow-sm"
+        }`}
+        onClick={() => handleClick()}
+      >
+        {active && (
+          <span className="absolute top-1/2 -translate-y-1/2 h-2 left-2 right-2 bg-cyan-300 blur opacity-50 brightness-150" />
+        )}
+        <span
+          className={`${active ? "text-cyan-600 blur-xs" : "text-slate-600"}`}
+        >
+          {item.name}
+        </span>
+        <span className="text-xs opacity-50">{keyPress}</span>
+      </button>
+      <div className="w-full h-2 bg-slate-400 -top-2 relative rounded-b-md" />
+    </div>
+  );
+}
